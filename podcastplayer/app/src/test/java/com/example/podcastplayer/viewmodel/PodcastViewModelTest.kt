c5b2941b7310a145c79c95baa6ac5624826e6c26
KiranZa <68339757+KiranZa@users.noreply.github.com>
2024-08-01 18:32:02 +0500

    Update PodcastViewModelTest.kt

--- podcastplayer/app/src/test/java/com/example/podcastplayer/viewmodel/PodcastViewModelTest.kt
+++ podcastplayer/app/src/test/java/com/example/podcastplayer/viewmodel/PodcastViewModelTest.kt
@@ -5,10 +5,12 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.podcastplayer.data.PodcastListResponse
 import com.example.podcastplayer.data.PodcastResponse
 import com.example.podcastplayer.network.PodcastApi
-
 import com.example.podcastplayer.respository.PodcastRepository
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.*
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,29 +25,38 @@ class PodcastViewModelTest {
 
     private lateinit var viewModel: PodcastViewModel
     private val repository: PodcastRepository = mock(PodcastRepository::class.java)
-    private val api: PodcastApi = mock(PodcastApi::class.java)
+    private val crashlytics: FirebaseCrashlytics = mock(FirebaseCrashlytics::class.java)
 
+    private val testDispatcher = StandardTestDispatcher()
 
     @Before
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
         MockitoAnnotations.openMocks(this)
-        viewModel = PodcastViewModel(repository)
+        viewModel = PodcastViewModel(repository, crashlytics)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
     }
 
     @Test
-    fun `loadPodcasts updates podcasts state`() = runTest {
+    fun loadPodcasts_updatesPodcastsState() = runTest {
         // Arrange
         val mockPodcasts = listOf(
             PodcastResponse(1, "Podcast 1", "Author 1", "image1.jpg", "Description"),
-            PodcastResponse(2, "Podcast 2", "Author 2", "image2.jpg","Description")
+            PodcastResponse(2, "Podcast 2", "Author 2", "image2.jpg", "Description")
         )
 
-
-
         val mockResponse = PodcastListResponse(mockPodcasts)
-        `when`(api.getTopPodcasts()).thenReturn(mockResponse)
+        `when`(repository.getTopPodcasts()).thenReturn(mockResponse)
+
         // Act
         viewModel.loadTopPodcasts()
+
+        // Advance time to ensure the coroutine completes
+        advanceUntilIdle()
 
         // Assert
         assert(viewModel.podcasts.value == mockPodcasts)
